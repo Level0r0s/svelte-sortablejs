e9b3235b802c99eb5c6a285bd45f1a21d77b51e4
RD_ <rachiduoad@gmail.com>
2019-12-31 14:34:03 +0100

    Work on docs

--- src/docs.svelte
+++ src/docs.svelte
@@ -4,6 +4,8 @@
   import { flip } from "svelte/animate";
   import { crossfade } from "svelte/transition";
 
+  import Logo from "./logo.svelte";
+
   const [send, receive] = crossfade({
     duration: d => Math.sqrt(d * 200),
 
@@ -60,13 +62,19 @@
 <style>
   #docs {
     display: flex;
+    flex-flow: column;
+    align-items: stretch;
+    justify-content: space-around;
+    height: 100%;
+  }
+
+  #todos {
+    display: flex;
+    flex-flow: row;
+    justify-content: space-between;
+    align-items: center;
   }
   #console-a {
-    white-space: pre;
-    font-family: monospace;
-    font-weight: bold;
-  }
-  #console-b {
     white-space: pre;
     font-family: monospace;
     font-weight: bold;
@@ -77,47 +85,57 @@
   :global(#sortable-b) {
     flex: 1;
   }
+  #console-b {
+    white-space: pre;
+    font-family: monospace;
+    font-weight: bold;
+  }
   .item {
     list-style: none;
     display: block;
     box-shadow: 0 0 0 2px black;
     padding: 5px;
+    user-select: none;
+    cursor: grab;
   }
 </style>
 
 <div id="docs">
-  <div id="console-a">
-    <button on:click={addtoList}>Add</button>
-    {JSON.stringify(list, 0, 4)}
-  </div>
-  <Sortable id="sortable-a" {options} bind:list>
-    {#each list as item (item.id)}
-      <li
-        in:receive={{ key: item.id }}
-        out:send={{ key: item.id }}
-        animate:flip={{ duration: 250 }}
-        data-id={item.id}
-        class="item"
-        style="background:{item.color};">
-        {item.name}
-      </li>
-    {/each}
-  </Sortable>
-  <Sortable id="sortable-b" options={options2} bind:list={list2}>
-    {#each list2 as item (item.id)}
-      <li
-        in:receive={{ key: item.id }}
-        out:send={{ key: item.id }}
-        animate:flip={{ duration: 250 }}
-        data-id={item.id}
-        class="item"
-        style="background:{item.color};">
-        {item.name}
-      </li>
-    {/each}
-  </Sortable>
-  <div id="console-b">
-    <button on:click={addtoList2}>Add</button>
-    {JSON.stringify(list2, 0, 4)}
+  <Logo />
+  <div id="todos">
+    <div id="console-a">
+      <button on:click={addtoList}>Add</button>
+      {JSON.stringify(list, 0, 4)}
+    </div>
+    <Sortable id="sortable-a" {options} bind:list>
+      {#each list as item (item.id)}
+        <li
+          in:receive={{ key: item.id }}
+          out:send={{ key: item.id }}
+          animate:flip={{ duration: 250 }}
+          data-id={item.id}
+          class="item"
+          style="background:{item.color};">
+          {item.name}
+        </li>
+      {/each}
+    </Sortable>
+    <Sortable id="sortable-b" options={options2} bind:list={list2}>
+      {#each list2 as item (item.id)}
+        <li
+          in:receive={{ key: item.id }}
+          out:send={{ key: item.id }}
+          animate:flip={{ duration: 250 }}
+          data-id={item.id}
+          class="item"
+          style="background:{item.color};">
+          {item.name}
+        </li>
+      {/each}
+    </Sortable>
+    <div id="console-b">
+      <button on:click={addtoList2}>Add</button>
+      {JSON.stringify(list2, 0, 4)}
+    </div>
   </div>
 </div>
---
+++ src/logo.svelte
@@ -0,0 +1,53 @@
+<script>
+
+</script>
+
+<style>
+  #logo {
+    display: flex;
+    flex-flow: column;
+    align-items: flex-start;
+    align-self: center;
+  }
+  .segment {
+    text-align: center;
+    padding: 5px 10px;
+    font-size: 35px;
+    font-weight: 100;
+    color: white;
+    letter-spacing: 10px;
+    user-select: none;
+  }
+  .segment:nth-of-type(1) {
+    background: #ff3e00;
+    transform: perspective(70px) rotateY(-5deg);
+    top: 3px;
+    z-index: 3;
+    position: relative;
+  }
+  .segment:nth-of-type(2) {
+    background: #d33906;
+    transform: perspective(70px) rotateY(16deg);
+    z-index: 2;
+  }
+  .segment:nth-of-type(2)::before {
+    background: rgba(0, 0, 0, 0.1);
+    content: "";
+    position: absolute;
+    top: -40px;
+    left: 11px;
+    width: 94px;
+    height: 47px;
+  }
+  .segment:nth-of-type(3) {
+    background: #b63409;
+    z-index: 1;
+    transform: perspective(70px) rotateY(-15deg);
+  }
+</style>
+
+<div id="logo">
+  <div class="segment">SOR</div>
+  <div class="segment">TAB</div>
+  <div class="segment">LE</div>
+</div>
